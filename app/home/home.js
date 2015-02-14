6f219c9049533fd55c4f6787313cf2e60d0ae140
zmhassan <zak.hassan1010@gmail.com>
2015-02-13 23:44:36 -0500

    Updated and completed home.html

--- app/home/home.js
+++ app/home/home.js
@@ -2,11 +2,22 @@
 
 (function(){
 
-  var HomeCtrl = function( $scope ) {
+  var HomeCtrl = function( $scope, $location ) {
     $scope.country= "Canada";
     $scope.title="";
     $scope.meta="";
     $scope.prefix="CA/";
+    $scope.fetchSearch=function fetchSearch() {
+      var role= $scope.role || "";
+      var local= $scope.location || "";
+      if ($scope.country == "Canada") {
+        window.location.replace("http://www.bzcareer.com/CA/search?keyword=" + role + "&local=" + local);
+      } else {
+        window.location.replace("http://www.bzcareer.com/search?keyword=" + role + "&local=" + local);
+      }
+    }
+
+
   };
 
   angular.module('myApp.home', ['ngRoute'])

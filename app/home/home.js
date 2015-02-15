ba2e8d5812939741e354d3eeca32342ab479d453
zmhassan <zak.hassan1010@gmail.com>
2015-02-14 22:12:34 -0500

    Refactoring and utilizing directives to encapsulate code

--- app/home/home.js
+++ app/home/home.js
@@ -1,31 +1,57 @@
 'use strict';
 
-(function(){
+(function () {
 
-  var HomeCtrl = function( $scope, $location ) {
-    $scope.country= "Canada";
-    $scope.title="";
-    $scope.meta="";
-    $scope.prefix="CA/";
-    $scope.fetchSearch=function fetchSearch() {
-      var role= $scope.role || "";
-      var local= $scope.location || "";
-      if ($scope.country == "Canada") {
-        window.location.replace("http://www.bzcareer.com/CA/search?keyword=" + role + "&local=" + local);
-      } else {
-        window.location.replace("http://www.bzcareer.com/search?keyword=" + role + "&local=" + local);
-      }
-    }
+    var HomeCtrl = function ($scope, $location) {
+        $scope.country = "Canada";
+        $scope.title = "";
+        $scope.meta = "";
+        $scope.prefix = "CA/";
+        $scope.fetchSearch = function fetchSearch() {
+            var role = $scope.role || "";
+            var local = $scope.location || "";
+            if ($scope.country == "Canada") {
+                window.location.replace("http://www.bzcareer.com/CA/search?keyword=" + role + "&local=" + local);
+            } else {
+                window.location.replace("http://www.bzcareer.com/search?keyword=" + role + "&local=" + local);
+            }
+        }
 
 
-  };
+    };
 
-  angular.module('myApp.home', ['ngRoute'])
-      .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', {
-          templateUrl: 'home/home.html',
-          controller: 'HomeCtrl'
+    angular.module('myApp.home', ['ngRoute'])
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/', {
+                templateUrl: 'home/home.html',
+                controller: 'HomeCtrl'
+            });
+            //TODO: Configure the url to use $locationProvider.html5mode(true) .
+        }]).controller('HomeCtrl', HomeCtrl)
+        .directive('bzfeaturebox1', function () {
+        return {
+            templateUrl: "templates/featurebox1.html",
+            restrict: "E",
+            replace: true
+        }
+    }).directive('bzfeaturebox2', function () {
+        return {
+            templateUrl: "templates/featurebox2.html",
+            restrict: "E",
+            replace: true
+        }
+    }).directive('bzfeaturebox3', function () {
+        return {
+            templateUrl: "templates/featurebox3.html",
+            restrict: "E",
+            replace: true
+        }
+    }).directive('bzsearchbox', function () {
+            return {
+                templateUrl: "templates/searchbox.html",
+                restrict: "E",
+                replace: true
+            }
         });
-        //TODO: Configure the url to use $locationProvider.html5mode(true) .
-      }]).controller('HomeCtrl', HomeCtrl);
+
 }())

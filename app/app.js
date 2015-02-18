3593c7ff1283fa212c6d8739ab3dce1318ce9e2e
zmhassan <zak.hassan1010@gmail.com>
2015-02-17 19:24:02 -0500

    Added angularUi bootstrap

--- app/app.js
+++ app/app.js
@@ -8,7 +8,8 @@ angular.module('myApp', [
   'myApp.contact',
   'myApp.about',
   'myApp.maps',
-  'myApp.service'
+  'myApp.service',
+    'ui.bootstrap'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});

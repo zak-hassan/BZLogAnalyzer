4227343c3ce418dd88944be198860330166cdc30
zmhassan <zak.hassan1010@gmail.com>
2015-02-26 16:44:10 -0500

    Adding some mockups for rest autocomplete endpoint

--- bzcareer-platform/rest/src/main/java/com/bzcareer/paas/rest/JobService.java
+++ bzcareer-platform/rest/src/main/java/com/bzcareer/paas/rest/JobService.java
@@ -5,19 +5,35 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
  
-@Path("/jobservice/")
+@Path("/jobservice")
  public class JobService {
  
 	
 //TODO: Add some logic here to pull data from the persistence service via osgi
 	@GET
-    @Path("/jobsearch/")
-    public List<String> getCustomer( ) {
+    @Path("/jobs")
+	@Produces("application/json")
+	public List<String> getCustomer( ) {
         List<String> list= new ArrayList<String>();
        list.add("Hello");
        
        return list;
     }
+	
+	@GET
+	@Path("/autocomplete")
+	@Produces("application/json")
+	public List<String> getKeywords(@QueryParam("start_with") String starts_with){
+		List<String> list= new ArrayList<String>();
+		if(starts_with.equals("J"))
+		list.add("Java");
+		list.add("Janitor");
+		list.add("Java Developer");
+		
+		return list;
+	}
 	
 }

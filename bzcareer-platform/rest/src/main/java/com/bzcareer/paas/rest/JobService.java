2a07d5f3306572d4fcaa64c9c057cb4d74d49119
zmhassan <zak.hassan1010@gmail.com>
2015-02-26 15:22:19 -0500

    Fixed rest endpoint and now is deployed on cxf

--- bzcareer-platform/rest/src/main/java/com/bzcareer/paas/rest/JobService.java
+++ bzcareer-platform/rest/src/main/java/com/bzcareer/paas/rest/JobService.java
@@ -1,9 +1,23 @@
 package com.bzcareer.paas.rest;
 
-public class JobService {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+ 
+@Path("/jobservice/")
+ public class JobService {
+ 
 	
 //TODO: Add some logic here to pull data from the persistence service via osgi
-
+	@GET
+    @Path("/jobsearch/")
+    public List<String> getCustomer( ) {
+        List<String> list= new ArrayList<String>();
+       list.add("Hello");
+       
+       return list;
+    }
 	
 }

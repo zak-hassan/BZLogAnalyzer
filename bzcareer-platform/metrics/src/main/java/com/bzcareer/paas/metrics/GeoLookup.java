1d80bce3b3586ef7171357ae4244dc16f2e877cc
zmhassan <zak.hassan1010@gmail.com>
2015-02-26 23:01:32 -0500

    More changes

--- bzcareer-platform/metrics/src/main/java/com/bzcareer/paas/metrics/GeoLookup.java
+++ bzcareer-platform/metrics/src/main/java/com/bzcareer/paas/metrics/GeoLookup.java
@@ -7,13 +7,13 @@ import com.maxmind.geoip.LookupService;
 
 public class GeoLookup {
 
-	public void lookUpByIp() {
+	public void lookUpByIp(String ip) {
 		LookupService cl;
 		try {
 			cl = new LookupService("etc/GeoLiteCity.dat",
 					LookupService.GEOIP_MEMORY_CACHE
 							| LookupService.GEOIP_CHECK_CACHE);
-			Location location = cl.getLocation("52.0.37.122");
+			Location location = cl.getLocation( ip);
 		} catch (IOException e) {
  			e.printStackTrace();
 		}
--- bzcareer-platform/persistence/src/main/java/com/bzcareer/pass/persistence/DriverWrapper.java
+++ bzcareer-platform/persistence/src/main/java/com/bzcareer/pass/persistence/DriverWrapper.java
@@ -1,23 +1,31 @@
 package com.bzcareer.pass.persistence;
 
+import java.util.Date;
+
 import org.bson.Document;
 
-import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class DriverWrapper {
-	
+
 	public static void main(String[] args) {
-		MongoCollection<Document> collection = connect("JobPostsDB","myJobPosts");
+		Date start = new Date(System.currentTimeMillis());
+		MongoCollection<Document> collection = connect("ResultsIndexCanada",
+				"FinishedJobsIndexed");
 		for (Document document : collection.find()) {
 			System.out.println(document.toString());
 		}
+		System.out.println(" Done !!! ");
+		Date end = new Date();
+		System.out.println(" Started : " + (start));
+		System.out.println(" Ended : " + (end));
+
 	}
-	
+
 	static MongoCollection<Document> connect(String db, String collection) {
-		MongoClient mongoClient = new MongoClient("bzcareer.com", 28017);
+		MongoClient mongoClient = new MongoClient("bzcareer.com", 27017);
 		MongoDatabase database = mongoClient.getDatabase(db);
 		MongoCollection<Document> col = database.getCollection(collection);
 		return col;

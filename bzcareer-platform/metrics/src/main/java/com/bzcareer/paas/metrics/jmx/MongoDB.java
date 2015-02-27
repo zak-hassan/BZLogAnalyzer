176185975487e4aa3459f17796af3e614c5b3e27
zmhassan <zak.hassan1010@gmail.com>
2015-02-27 17:25:24 -0500

    Added mongo mbeans

--- bzcareer-platform/metrics/src/main/java/com/bzcareer/paas/metrics/jmx/MongoDB.java
+++ bzcareer-platform/metrics/src/main/java/com/bzcareer/paas/metrics/jmx/MongoDB.java
@@ -1,15 +1,54 @@
 package com.bzcareer.paas.metrics.jmx;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.bson.Document;
 
 import com.bzcareer.pass.persistence.DriverWrapper;
+import com.bzcareer.pass.persistence.Job;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 
 public class MongoDB implements MongoDBMBean {
 
+	public int TotalCompanies;
+	public int TotalJobs;
+	public Map<String,Integer> companyStats;
+	
+	public int getTotalCompanies(){
+		return this.TotalCompanies;
+	}
+	public int getTotalJobs(){
+		return this.TotalJobs;
+	}
+	public Map<String,Integer> getCompanyStats(){
+		return companyStats;
+	}
+	
+	public MongoDB() {
+		Date start = new Date(System.currentTimeMillis());
+		MongoCollection<Document> collection = DriverWrapper.connect("ResultsIndexCanada",
+				"FinishedJobsIndexed");
+		int count = 0;
+		Map<String, Integer> side = new HashMap<String, Integer>();
+
+		collection.count();
+		FindIterable<Document> found = collection.find();
+		
+		for (Document document : found) {
+			DriverWrapper.addUnique(side, document.get("CompanyName").toString());
+			count++;
+		}
+ 
+		System.out.println(" Total Sidebar: " + side);
+		System.out.println(" Total Companies: " + side.size());
+		companyStats=side;
+		TotalCompanies=side.size();
+		TotalJobs=count;
+	}
+	
 	public long getTotalJobs(String db, String collection) {
 //		MongoCollection<Document> col = DriverWrapper.connect("ResultsIndexCanada",
 	//			"FinishedJobsIndexed");
@@ -20,9 +59,5 @@ public class MongoDB implements MongoDBMBean {
 		return col.count();
 	}
 
-	public long getTotalCompanies() {
-		
-		return 0L;
-	}
-
+ 
 }
--- bzcareer-platform/metrics/src/main/java/com/bzcareer/paas/metrics/jmx/MongoDBMBean.java
+++ bzcareer-platform/metrics/src/main/java/com/bzcareer/paas/metrics/jmx/MongoDBMBean.java
@@ -1,8 +1,11 @@
 package com.bzcareer.paas.metrics.jmx;
 
+import java.util.Map;
+
 public interface MongoDBMBean {
- 
+	public int getTotalCompanies();
+	public int getTotalJobs();
+	public Map<String,Integer> getCompanyStats();
 	public long getTotalJobs(String db, String collection);
-	public long getTotalCompanies();
-	 
+	
 }

5dd1c85c11d29b547c59526206ceae41098e9c71
zmhassan <zak.hassan1010@gmail.com>
2015-02-26 19:27:33 -0500

    Adding more java

--- bzcareer-platform/persistence/src/main/java/com/bzcareer/pass/persistence/MongoDBService.java
+++ bzcareer-platform/persistence/src/main/java/com/bzcareer/pass/persistence/MongoDBService.java
@@ -15,6 +15,8 @@ public class MongoDBService {
 		// TODO: Get search results from MongoDB remotely and query the jobs
 		if (isValid(keyword) && isRealLocation(local)) {
 
+			
+			MongoCollection<Document> 	collection = Driver.connect("CAUrlDB", "newCanadaMap");
 		}
 		return new ArrayList<SearchResults>();
 	}
@@ -27,15 +29,26 @@ public class MongoDBService {
 	}
 
 	private boolean isValid(String keyword) {
-		// TODO Make sure the the keyword is not undefined or null
-		// TODO Check for null
+		// TODO Make sure the the keyword is not undefined or null ...
+		// TODO Check for null ...
+		// TODO Also check if this is a synonyms of the word ...
 		return true;
 	}
 
 	public List<SearchResults> queryByCompany(String company, String role,
 			String location, int page) {
+		//TODO: When queries come in this is a result of 'company:*' in the query string keyword param.
+		
 		return new ArrayList<SearchResults>();
 	}
+
+	public List<SearchResults> queryByJobType(String jobType, String role,
+			String location, int page) {
+		//TODO: When queries come in this is a result of 'company:*' in the query string keyword param.
+		return new ArrayList<SearchResults>();
+	}
+
+ 
 
 	public List<String> queryAutocomplete(String starts_with, String country) {
 

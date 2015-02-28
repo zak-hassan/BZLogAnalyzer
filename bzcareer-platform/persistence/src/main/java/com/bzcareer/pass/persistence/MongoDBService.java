edc8c1f955ddec1783def61d21241bd12d18567b
zmhassan <zak.hassan1010@gmail.com>
2015-02-28 18:30:24 -0500

    Adding more search logic

--- bzcareer-platform/persistence/src/main/java/com/bzcareer/pass/persistence/MongoDBService.java
+++ bzcareer-platform/persistence/src/main/java/com/bzcareer/pass/persistence/MongoDBService.java
@@ -18,76 +18,69 @@ public class MongoDBService {
 	public SearchResults query(String keyword, String local, int num) {
 		// TODO: Get search results from MongoDB remotely and query the jobs
 		 SearchResults results=new SearchResults();
-		if (isValid(keyword) && isRealLocation(local)) {
-			MongoCollection<Document> 	collection = DriverWrapper.connect("CAUrlDB", "newCanadaMap");
-
-			
+		if (isValid(keyword) && isRealLocation(local)) { 
 			Date start = new Date(System.currentTimeMillis());
 			MongoCollection<Document> collection = DriverWrapper.connect(
 					"ResultsIndexCanada", "FinishedJobsIndexed");
 			int count = 0;
-			Map<String, Integer> side = new HashMap<String, Integer>();
-
+			Map<String, Integer> sideCompany = new HashMap<String, Integer>();
+			Map<String, Integer> sideJType = new HashMap<String, Integer>();
 			collection.count();
 			FindIterable<Document> found = collection.find();
-
 			for (Document document : found) {
-				System.out.println("Contains Company Name: " + document.keySet());
-				DriverWrapper.addUnique(side, document.get("CompanyName")
+ 				DriverWrapper.addUnique(sideCompany, document.get("CompanyName")
 						.toString());
-				
-				Job job = new Job();
-				if (document.keySet().contains("CompanyName")) {
-					job.setCompanyName(document.get("CompanyName").toString());
-				}
-				if (document.keySet().contains("JobDate")) {
-					job.setJobDate(document.get("JobDate").toString());
-				}
-				if (document.keySet().contains("JobDetails")) {
-					String jdescribe = document.get("JobDetails").toString();
-					if (jdescribe.length() < 120) {
-						job.setJobDetails(document.get("JobDetails").toString());
-					} else {
-						job.setJobDetails(document.get("JobDetails").toString()
-								.substring(0, 120)
-								+ "...");
-					}
-				}
-				if (document.keySet().contains("JobDetailURL")) {
-					job.setJobDetailURL(document.get("JobDetailURL").toString());
-				}
-				if (document.keySet().contains("JobID1")) {
-					job.setJobID1(document.get("JobID1").toString());
-				}
-				if (document.keySet().contains("JobLocation")) {
-					job.setJobLocation(document.get("JobLocation").toString());
-				}
-				if (document.keySet().contains("JobTitle")) {
-					job.setJobTitle(document.get("JobTitle").toString());
-				}
-				if (document.keySet().contains("JobType")) {
-					job.setJobType(document.get("JobType").toString());
-				}
-
-				System.out.println(job);
-				count++;
+ 				DriverWrapper.addUnique(sideJType, document.get("JobType")
+						.toString());
+ 				
+				Job job = initJob(document);
+				// TODO: do threading logic to make this run faster then 15-30 seconds.
 				 results.addJob(job);
-
 			}
-			System.out.println(" Done !!! ");
-			Date end = new Date();
-			System.out.println(" Started : " + (start));
-			System.out.println(" Ended : " + (end));
-			System.out.println(" Total Jobs: " + count);
-			System.out.println(" Total Sidebar: " + side);
-			System.out.println(" Total Companies: " + side.size());
-			Sidebar sidebar = new Sidebar();
-			sidebar.setCompanies(side);
-			results.setSideBar();
-			
-			
-			
+ 			Date end = new Date();
+		 	Sidebar sidebar = new Sidebar();
+			sidebar.setCompanies(sideCompany);;
+			sidebar.setJobType(sideJType);
+			results.setSideBar(sidebar);
 		}
+		return results;
+	}
+
+	private Job initJob(Document document) {
+		Job job =new Job();
+		
+		if (document.keySet().contains("CompanyName")) {
+			job.setCompanyName(document.get("CompanyName").toString());
+		}
+		if (document.keySet().contains("JobDate")) {
+			job.setJobDate(document.get("JobDate").toString());
+		}
+		if (document.keySet().contains("JobDetails")) {
+			String jdescribe = document.get("JobDetails").toString();
+			if (jdescribe.length() < 120) {
+				job.setJobDetails(document.get("JobDetails").toString());
+			} else {
+				job.setJobDetails(document.get("JobDetails").toString()
+						.substring(0, 120)
+						+ "...");
+			}
+		}
+		if (document.keySet().contains("JobDetailURL")) {
+			job.setJobDetailURL(document.get("JobDetailURL").toString());
+		}
+		if (document.keySet().contains("JobID1")) {
+			job.setJobID1(document.get("JobID1").toString());
+		}
+		if (document.keySet().contains("JobLocation")) {
+			job.setJobLocation(document.get("JobLocation").toString());
+		}
+		if (document.keySet().contains("JobTitle")) {
+			job.setJobTitle(document.get("JobTitle").toString());
+		}
+		if (document.keySet().contains("JobType")) {
+			job.setJobType(document.get("JobType").toString());
+		}
+		return job;
 	}
 
 	private boolean isRealLocation(String local) {
@@ -104,17 +97,16 @@ public class MongoDBService {
 		return true;
 	}
 
-	public List<SearchResults> queryByCompany(String company, String role,
+	public  SearchResults queryByCompany(String company, String role,
 			String location, int page) {
 		//TODO: When queries come in this is a result of 'company:*' in the query string keyword param.
-		
-		return new ArrayList<SearchResults>();
+		return new SearchResults();
 	}
 
-	public List<SearchResults> queryByJobType(String jobType, String role,
+	public  SearchResults queryByJobType(String jobType, String role,
 			String location, int page) {
 		//TODO: When queries come in this is a result of 'company:*' in the query string keyword param.
-		return new ArrayList<SearchResults>();
+		return new  SearchResults();
 	}
 
  
